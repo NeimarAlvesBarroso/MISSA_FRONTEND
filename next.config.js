5ad1fe682ca6e776935d92a5557b6b913833d721
NeimarAlvesBarroso <neimarab@gmail.com>
2025-05-04 08:33:22 -0300

    Publicar código frontend

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      allowedDevOrigins: ["http://10.0.0.104:3000"], // Ajuste conforme necessário
+    },
+  };
+  
+  module.exports = nextConfig;
+  "" 
